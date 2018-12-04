b97e8a902cc8c216a48b445b062a1fff73b76b19
Mai Văn Toàn <toan.mv0823@gmail.com>
2018-12-04 15:46:26 +0700

    Methods trong Vue

---
+++ resources/js/components/MyComponent.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="my-component">
+        <div>{{ message }}</div>
+        <div>
+            <button @click="sayHello">Click me!</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                message: 'This is my first component using binding data'
+            }
+        },
+        methods: {
+            sayHello() {
+                this.message = "You clicked button"
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+    .my-component {
+        color: red;
+    }
+</style>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,7 +7,7 @@
     </head>
     <body>
         <div id="app">
-            <example-component></example-component>
+            <my-component></my-component>
         </div>
         <script src="/js/app.js"></script>
     </body>
